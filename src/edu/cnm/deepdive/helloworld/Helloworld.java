087da8f7f1c8c4e043b3476959bc5e0ed721c471
Lobo_ <Lobo_@DESKTOP-OTJ6HMR.Home>
2019-01-03 09:43:06 -0700

    No major functional changes, first project in deep dive

---
+++ src/edu/cnm/deepdive/helloworld/Helloworld.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * simple class that displays the "Hello World!" message.
+ */
+public class Helloworld {
+
+  /**
+   * Prints "Hello World!" to standard output.
+   * 
+   * @param args    Command-line parameters (ignored).
+   */
+  public static void main(String[] args) {
+   System.out.println("Hello World!");
+
+  }
+
+}
